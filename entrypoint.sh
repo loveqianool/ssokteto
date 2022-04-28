8b09a8d342356d67ef4f1316bff644817c8c3b06
zhuYe <loveqianool@users.noreply.github.com>
2022-04-28 22:15:55 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,51 +1,29 @@
 #!/bin/sh
-
-# Global variables
-DIR_CONFIG="/etc/v2ray"
-DIR_RUNTIME="/usr/bin"
-DIR_TMP="$(mktemp -d)"
-
-ID=ad806487-2d26-4636-98b6-ab85cc8521f7
-AID=64
-WSPATH=/
+PASSWORD=123123
+WSPATH=/TS/recharge/tzUrl.html
 PORT=80
 
-# Write V2Ray configuration
-cat << EOF > ${DIR_TMP}/heroku.json
+cat << EOF > /etc/shadowsocks-rust/config.json
 {
-    "inbounds": [{
-        "port": ${PORT},
-        "protocol": "vmess",
-        "settings": {
-            "clients": [{
-                "id": "${ID}",
-                "alterId": ${AID}
-            }]
-        },
-        "streamSettings": {
-            "network": "ws",
-            "wsSettings": {
-                "path": "${WSPATH}"
-            }
+    "servers": [
+        {
+            "address": "0.0.0.0",
+            "port": ${PORT},
+            "password": "${PASSWORD}",
+            "method": "chacha20-ietf-poly1305",
+            "fast_open": true,
+            "prefer_ipv6": false,
+            "no_delay": true,
+            "nofile": 10240,
+            "keep_alive": 6,
+            "mode": "tcp_and_udp",
+            "dns": "1.1.1.1",
+            "plugin":"v2ray-plugin",
+            "plugin_opts":"server;path=${WSPATH};loglevel=none"
         }
-    }],
-    "outbounds": [{
-        "protocol": "freedom"
-    }]
+    ]
 }
 EOF
 
-# Get V2Ray executable release
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
-busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
-
-# Convert to protobuf format configuration
-mkdir -p ${DIR_CONFIG}
-${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
-
-# Install V2Ray
-install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
-rm -rf ${DIR_TMP}
-
 # Run V2Ray
-${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
+/usr/bin/ssserver --log-without-time -c /etc/shadowsocks-rust/config.json
